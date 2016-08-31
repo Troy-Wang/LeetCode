db0df09ee245f7550221ad8fb3a98202d37eb4de
troy.wang <wangzhijian22@gmail.com>
2016-08-31 16:23:23 +0800

    2016-08-30

---
+++ Week1_201609/20160830_2.py
@@ -0,0 +1,19 @@
+"""Given an array of integers, every element appears twice except for one.
+Find that single one.Given an array of integers, every element appears twice except for one.
+ Find that single one."""
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for eachNum in nums:
+            result ^= eachNum
+        return result
+
+
+solution = Solution()
+print solution.singleNumber([2])
