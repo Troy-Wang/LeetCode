d70501aa8c5cec542b6b5cb5efb7a1d1725ed47b
troy.wang <wangzhijian22@gmail.com>
2016-09-02 17:54:05 +0800

    2016-09-02

---
+++ Week1_201609/20160902_1.py
@@ -0,0 +1,24 @@
+"""
+Given an array nums, write a function to move all 0's to the end of it
+while maintaining the relative order of the non-zero elements.
+"""
+
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        count = 0
+        for i, j in enumerate(nums):
+            if j != 0:
+                nums[i] = 0
+                nums[count] = j
+                count += 1
+
+
+solution = Solution()
+nums = [0, 1, 2, 0, 3, 0, 4, 5, 6, 0, 0, 0]
+solution.moveZeroes(nums)
+print nums
