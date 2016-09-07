aff91f34e7236eea84e117f602304c58711f0dfa
troy.wang <wangzhijian22@gmail.com>
2016-09-07 18:56:56 +0800

    2016-09-07

---
+++ Week2_201609/20160907_3.py
@@ -0,0 +1,30 @@
+"""
+Given two arrays, write a function to compute their intersection.
+"""
+
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        aDict = {}
+        for eachNum in nums1:
+            if aDict.has_key(eachNum):
+                aDict[eachNum] += 1
+            else:
+                aDict[eachNum] = 1
+
+        ret = []
+        for eachNum in nums2:
+            if aDict.has_key(eachNum) and aDict[eachNum] > 0:
+                ret.append(eachNum)
+                aDict[eachNum] -= 1
+
+        return ret
+
+
+solution = Solution()
+print solution.intersect([1, 2], [1])
