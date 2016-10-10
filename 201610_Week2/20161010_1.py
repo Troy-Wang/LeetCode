5a6ce923a5a63a065456df7154e823010b526e59
troy.wang <wangzhijian22@gmail.com>
2016-10-11 00:10:18 +0800

    2016-10-11

---
+++ 201610_Week2/20161010_1.py
@@ -0,0 +1,40 @@
+"""
+Sum of Left Leaves
+Find the sum of all left leaves in a given binary tree.
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        elif root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        else:
+            return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+
+
+a = TreeNode(3)
+b = TreeNode(9)
+c = TreeNode(20)
+d = TreeNode(15)
+e = TreeNode(7)
+a.left = b
+a.right = c
+c.left = d
+c.right = e
+
+solution = Solution()
+print(solution.sumOfLeftLeaves(a))
